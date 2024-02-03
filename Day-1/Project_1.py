b1590211bb1605fedd0499e4585a58f90bee93a6
Deepak <dk5007667@gmail.com>
2024-02-04 00:03:25 +0530

    Day one complete

---
+++ Day-1/Project_1.py
@@ -0,0 +1,14 @@
+# Display a welcoming message for the user
+print("Welcome to the Band Name Generator")
+
+# Ask the user to input the name of the city they grew up in
+city_name = input("What is the name of the city you grew up in?\n")
+
+# Ask the user to input the name of their pet
+pet_name = input("What is the name of your pet?\n")
+
+# Combine the city name and pet name to create a band name
+band_name = city_name + " " + pet_name
+
+# Display the generated band name to the user
+print("Your band name could be " + band_name)
---
+++ Day-1/Readme.txt
@@ -0,0 +1,15 @@
+Day 1: Hello Python World! 🐍
+
+Today's Progress:
+
+Printed 'Hello, World!' in Python.
+Explored the basics of variables and their usage.
+Delved into strings and string manipulation techniques.
+Created a fun project - Band Name Generator.
+
+Thoughts:
+
+Exciting start to #100DaysOfCode! 🚀 Python fundamentals are taking shape. The Band Name Generator project was a blast! Ready for more challenges and learning. 💻💡
+
+Project:
+Check out my Band Name Generator project source code below !
